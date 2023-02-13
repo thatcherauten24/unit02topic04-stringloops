a5cbf6afcfae3e1554b6e3b41b6eab1c9ca140a5
Thatcher Auten <thatcherauten24@gmail.com>
2023-02-13 12:30:41 -0600

    indexes of all; not complete

--- StringLoops.java
+++ StringLoops.java
@@ -1,10 +1,27 @@
-public class Loops {
+import java.util.Arrays;
+public class StringLoops {
 
-  // static methods go here
+public static String reverseCharacters(String str) {
+  int index = str.length();
+  String str2 = "";
+  for (index = str.length() - 1; index >= 0; index --) {
+    str2 += str.substring(index, index + 1);
+  }
+  return str2;
+}
 
+public static void indexesOfAll(String text, String searchTerm){
+  int[] appearances = new int[] {};
+  int index = 0;
+  for (index = 0; index <= text.length(); index ++) {
+    if (searchTerm.equals(text.substring(index, index + 1))) {
+      appearances[index] = index;
+    }
+  }
+  Arrays.toString(appearances);
+} 
   public static void main(String[] args) {
-
-    // testing of static methods goes here
-
+indexesOfAll("attack", "a");
+  
   }
 }
