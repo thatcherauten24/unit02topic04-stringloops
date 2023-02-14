fd488001d2e05f456edb3cb702e803906e6806d7
Thatcher Auten <thatcherauten24@gmail.com>
2023-02-14 12:31:18 -0600

    beginning of reverseWords

--- StringLoops.java
+++ StringLoops.java
@@ -10,7 +10,7 @@ public static String reverseCharacters(String str) {
   return str2;
 }
 
-public static int[] indexesOfAll(String text, String searchTerm){
+public static int[] indexesOfAll(String text, String searchTerm) {
   int index = 0;
   int total = 0;
   for (index = 0; index < text.length(); index ++) {
@@ -28,9 +28,34 @@ public static int[] indexesOfAll(String text, String searchTerm){
   }
   return appearances;
 }
+
+public static boolean hasRepeatedConsecutives(String word) {
+  boolean twoLetters = false;
+  int index = 0;
+  for (index = 0; index < word.length() - 1; index ++) {
+    if (word.substring(index, index + 1).equals(word.substring(index + 1, index + 2))) {
+      twoLetters = true;
+    }
+  }
+
+
+
+
+  return twoLetters;
+}
+
+public static String reverseWords(String phrase) {
+  int index = phrase.length();
+  for (index = phrase.length(); index >= 0; index--) {
+
+  }
+
+}
   public static void main(String[] args) {
     //System.out.println(reverseCharacters("academy"));
     //System.out.println(Arrays.toString(indexesOfAll("attack", "t")));
+    //System.out.println(hasRepeatedConsecutives("bookkeeper"));
+    System.out.println(reverseWords("the quick brown fox"));
     
   }
 }
